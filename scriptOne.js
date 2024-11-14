07f6a485f2d30548b86716b5d4094d3756a7d952
Sporklife <klachanaris1442@gmail.com>
2024-11-14 10:42:04 -0500

    lab8done

---
+++ scriptOne.js
@@ -0,0 +1,92 @@
+
+
+let myname = "kypros";
+let age = 18;
+console.log("Name:", myname);
+console.log("Age:", age);
+
+let string = "fwefwf";
+let number = 12342;
+let boolean = true;
+let BigInt = 4;
+console.log("string:", string);
+console.log("number:", number);
+console.log("boolean:", boolean);
+console.log("BigInt:", BigInt);
+
+
+let rollNumber = 20;
+if (rollNumber > 10) {
+    console.log("greater than 10");
+}
+
+
+let ismember = true;
+if (ismember) {
+    console.log("member discount applied ");
+
+}
+
+
+let startValue = 50;
+if (startValue > 0) {
+    console.log("positive");
+} else if (startValue < 0) {
+    console.log(" negative");
+} else {
+    console.log("zero");
+}
+
+
+let a = 5;
+let b = 10;
+
+console.log(" == ", a == b);
+console.log(" === ", a === b);
+console.log(" != ", a != b);
+console.log(" > ", a > b);
+console.log(" < ", a < b);
+console.log(" >= ", a >= b);
+console.log(" <= ", a <= b);
+
+
+
+let firstname = "kypros";
+let lastname = "lachanaris";
+let age2 = 18;
+console.log( "Hello, " + firstname + lastname + ", you are " + age + " years old. ");
+
+
+let num = 50;
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+}
+
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} else {
+    console.log("Check your driving eligibility.");
+}
+
+
+
+num = 21;
+if (num % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A.");
+} else if (score >= 80 && score <= 89) {
+    console.log("Grade B.");
+} else {
+    console.log("Grade C.");
+}
+
